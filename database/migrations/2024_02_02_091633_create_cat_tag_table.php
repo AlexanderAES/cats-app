f757d8622b48912073d53af31b821a97ac1bbf9b
Alexander Suetnov <aes123.aa@gmail.com>
2024-02-02 15:15:29 +0300

    refactor: оптимизация файла миграции create_tag_table Улучшена структура файла миграции для повышения читаемости.

--- database/migrations/2024_02_02_091633_create_cat_tag_table.php
+++ database/migrations/2024_02_02_091633_create_cat_tag_table.php
@@ -4,20 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::create('cat_tag', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedBigInteger('cat_id');
-            $table->foreign('cat_id')->references('id')->on('cats');
-            $table->unsignedBigInteger('tag_id');
-            $table->foreign('tag_id')->references('id')->on('tags');
-            $table->timestamps();
+            $table->foreignId('cat_id')->constrained();
+            $table->foreignId('tag_id')->constrained();
         });
     }
 
