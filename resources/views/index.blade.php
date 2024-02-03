3a9c1c4bb5310833c4e91da0f58df6eeedf82352
Alexander Suetnov <aes123.aa@gmail.com>
2024-02-03 12:42:45 +0300

    feat:сделал вывод на главную страницу последних шести добавленных кошек

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,124 +3,31 @@
 @section('content')
     <article class="promo">
         <div class="container">
-            <div class="promo__descr">
-                <div class="promo__sorting">Сортировать по:</div>
-                <div class="promo__sorting-price">Цене</div>
-                <div class="promo__sorting-age">Возрасту</div>
-            </div>
             <div class="promo__cats">
-                <div class="promo__card promo-sale">
-                    <span class="notification"></span>
-                    <div class="promo__img">
-                        <img src="{{asset('images/cat_1.jpg')}}" alt="cat">
-                    </div>
-                    <div class="promo__inner">
-                        <h2 class="promo__name">Сибирская</h2>
-                        <div class="promo__box">
-                            <div class="promo__color">Коричневый окрас</div>
-                            <div class="promo__age">
-                                <div class="promo__age-month">2мес.</div>
-                                <div class="promo__age-text">Возраст</div>
-                            </div>
-                            <div class="promo__count">
-                                <div class="promo__count-num">самец</div>
-                                <div class="promo__count-text">Пол</div>
-                            </div>
+                @foreach($cats as $cat)
+                    <div class="promo__card promo-sale">
+                        <span class="notification"></span>
+                        <div class="promo__img">
+                            <img src="{{ asset($cat->img[0]) }}" alt="cat">
                         </div>
-                        <div class="promo__price">30 000 руб.</div>
-                    </div>
-                    <button class="promo__btn">Купить</button>
-                </div>
-                <div class="promo__card">
-                    <span class="notification"></span>
-                    <div class="promo__img">
-                        <img src="{{asset('images/cat_7.jpg')}}" alt="cat">
-                    </div>
-                    <div class="promo__inner">
-                        <h2 class="promo__name">Сибирская</h2>
-                        <div class="promo__box">
-                            <div class="promo__color">Коричневый окрас</div>
-                            <div class="promo__age">
-                                <div class="promo__age-month">2мес.</div>
-                                <div class="promo__age-text">Возраст</div>
+                        <div class="promo__inner">
+                            <h2 class="promo__name">{{$cat->breed}}</h2>
+                            <div class="promo__box">
+                                <div class="promo__color">{{$cat->color}}</div>
+                                <div class="promo__age">
+                                    <div class="promo__age-month">{{$cat->age}} мес.</div>
+                                    <div class="promo__age-text">Возраст</div>
+                                </div>
+                                <div class="promo__count">
+                                    <div class="promo__count-num">{{$cat->sex}}</div>
+                                    <div class="promo__count-text">Пол</div>
+                                </div>
                             </div>
-                            <div class="promo__count">
-                                <div class="promo__count-num">самка</div>
-                                <div class="promo__count-text">Пол</div>
-                            </div>
+                            <div class="promo__price">{{$cat->price}} руб.</div>
                         </div>
-                        <div class="promo__price">40 000 руб.</div>
+                        <button class="promo__btn">Купить</button>
                     </div>
-                    <button class="promo__btn" disabled>Продан</button>
-                </div>
-                <div class="promo__card">
-                    <span class="notification"></span>
-                    <div class="promo__img">
-                        <img src="{{asset('images/cat_3.jpg')}}" alt="cat">
-                    </div>
-                    <div class="promo__inner">
-                        <h2 class="promo__name">Сибирская</h2>
-                        <div class="promo__box">
-                            <div class="promo__color">Коричневый окрас</div>
-                            <div class="promo__age">
-                                <div class="promo__age-month">2мес.</div>
-                                <div class="promo__age-text">Возраст</div>
-                            </div>
-                            <div class="promo__count">
-                                <div class="promo__count-num">самец</div>
-                                <div class="promo__count-text">Пол</div>
-                            </div>
-                        </div>
-                        <div class="promo__price">20 000 руб.</div>
-                    </div>
-                    <button class="promo__btn">Купить</button>
-                </div>
-                <div class="promo__card">
-                    <span class="notification"></span>
-                    <div class="promo__img">
-
-                        <img src="{{asset('images/cat_4.jpg')}}" alt="cat">
-                    </div>
-                    <div class="promo__inner">
-                        <h2 class="promo__name">Сибирская</h2>
-                        <div class="promo__box">
-                            <div class="promo__color">Коричневый окрас</div>
-                            <div class="promo__age">
-                                <div class="promo__age-month">2мес.</div>
-                                <div class="promo__age-text">Возраст</div>
-                            </div>
-                            <div class="promo__count">
-                                <div class="promo__count-num">самка</div>
-                                <div class="promo__count-text">Пол</div>
-                            </div>
-                        </div>
-                        <div class="promo__price">25 000 руб.</div>
-                    </div>
-                    <button class="promo__btn">Купить</button>
-                </div>
-                <div class="promo__card promo-sale">
-                    <span class="notification"></span>
-                    <div class="promo__img">
-
-                        <img src="{{asset('images/cat_5.jpg')}}" alt="cat">
-                    </div>
-                    <div class="promo__inner">
-                        <h2 class="promo__name">Сибирская</h2>
-                        <div class="promo__box">
-                            <div class="promo__color">Коричневый окрас</div>
-                            <div class="promo__age">
-                                <div class="promo__age-month">2мес.</div>
-                                <div class="promo__age-text">Возраст</div>
-                            </div>
-                            <div class="promo__count">
-                                <div class="promo__count-num">самец</div>
-                                <div class="promo__count-text">Пол</div>
-                            </div>
-                        </div>
-                        <div class="promo__price">30 000 руб.</div>
-                    </div>
-                    <button class="promo__btn">Купить</button>
-                </div>
+                @endforeach
                 <div class="promo__card">
                     <span class="notification"></span>
                     <div class="promo__img">
@@ -145,7 +52,6 @@
                     <button class="promo__btn" disabled>Продан</button>
                 </div>
             </div>
-            <button class="promo__button">Показать еще 20</button>
         </div>
     </article>
 @endsection
