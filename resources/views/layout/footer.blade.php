891ea8a84fd80005c2507cce59f12b439d7883c0
Alexander Suetnov <aes123.aa@gmail.com>
2024-02-03 17:31:57 +0300

    feat: реализовал отправку данных с формы

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -6,18 +6,16 @@
                 <p class="footer__text">Подпишись и успей поучавсвовать в акции</p>
             </div>
             <div class="footer__subscription">
-                <form class="footer__form">
+                <form class="footer__form" method="POST" action="/subscribe">
+                    @method('POST')
+                    @csrf
                     <div class="footer__form-box">
                         <label for="email">
                             <input id="email" type="email" name="email" placeholder="email">
                         </label>
                         <label for="submit-btn">
-                            <input id="submit-btn" type="submit" name="submit" value="Подписаться">
+                            <input id="submit-btn" type="submit" name="submit" value="Подписаться на новости">
                         </label>
-                    </div>
-                    <div class="footer__form-checkbox">
-                        <input type="checkbox" name="checkbox" id="check">
-                        <label for="check">Подписаться на новости</label>
                     </div>
                 </form>
             </div>
